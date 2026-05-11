c98474a441eea905091940661ba9067522e0bd4f
coyninja4 <cyoshihara4@gmail.com>
2026-05-10 21:24:01 -0700

    Add files via upload

---
+++ LapCounter.py
@@ -0,0 +1,28 @@
+from time import sleep
+from datetime import date
+
+today = date.today()
+stop = False
+addedlap = False
+tags = {}
+
+print("read instructions in jogathon lap counter google drive folder before use ")
+sleep(1)
+
+while stop == False:
+    key = input("waiting: ")
+    if key == "stop":
+        stop = True
+        break
+    elif key in tags:
+        #gets list stored in dict and stores as data
+        data = tags[key]
+        data[1] += 1
+        print(f"name: {data[0]}, laps: {data[1]}")
+    else:
+        #assigns tag id in dict with list containing name and starts at lap 0
+        tags[key] = list((input("Enter name: "), 0))
+f = open(f"laps{today}.txt", 'x')
+for i in tags:
+    with open(f"{f}", "a") as f:
+        f.write(f"{i}")
